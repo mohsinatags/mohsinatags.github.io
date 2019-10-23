d9f3a16135e39068913a02212288a15d66dc0c89
mohsinatags <35526395+mohsinatags@users.noreply.github.com>
2019-10-23 20:30:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ Object.assign=require('object-assign')
 //app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 //app.use('/', index);
-
+app.use(morgan());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
