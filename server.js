291e4fcc8927e88d5660b347985c7132be428c7f
mohsinatags <35526395+mohsinatags@users.noreply.github.com>
2019-10-23 18:36:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express'),
     morgan  = require('morgan');
     
 Object.assign=require('object-assign')
-app.set('views', __dirname);
+
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
